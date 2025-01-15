888001d27e52ca49a19ff28f30ea88226a24c9e3
Luis Felipe Aricapa Muñoz <laricapa@unal.edu.co>
2025-01-15 10:50:20 -0500

    LAB7

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -16,3 +16,97 @@ def pregunta_01():
     El gráfico debe salvarse al archivo `files/plots/news.png`.
 
     """
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    import os
+
+    os.makedirs('files/input/plots', exist_ok=True)
+
+    dataset = pd.read_csv('files/input/news.csv', sep=',', index_col=0)
+
+    colors = {
+        'Television': 'dimgray',
+        'Newspaper': 'grey',
+        'Internet': 'tab:blue',
+        'Radio': 'lightgrey'
+    }
+
+    zorder = {
+        'Television': 1,
+        'Newspaper': 1,
+        'Internet': 2,
+        'Radio': 1
+    }
+
+    linewidths = {
+        'Television': 2,
+        'Newspaper': 2,
+        'Internet': 3,
+        'Radio': 2
+    }
+
+    fig, ax = plt.subplots()
+
+    for column in dataset.columns:
+        ax.plot(
+            dataset[column],
+            label=column,
+            color=colors[column],
+            zorder=zorder[column],
+            linewidth=linewidths[column],
+        )
+
+    ax.set_title('How people get their news', fontsize=16)
+    ax.spines['top'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    for column in dataset.columns:
+        first_year = dataset.index[0]
+        ax.scatter(
+            x=first_year,
+            y=dataset[column][first_year],
+            color=colors[column]
+        )
+
+        ax.text(
+            first_year - 0.25,
+            dataset[column][first_year],
+            column + " " + str(dataset[column][first_year]) + '%',
+            ha='right',
+            va='center',
+            color=colors[column]
+
+        )
+
+        last_year = dataset.index[-1]
+        ax.scatter(
+            x=last_year,
+            y=dataset[column][last_year],
+            color=colors[column]
+        )
+
+        ax.text(
+        last_year + 0.25,
+        dataset[column][last_year],
+        str(dataset[column][last_year]) + '%',
+        ha='left',
+        va='center',
+        color=colors[column]
+        )
+
+    ax.set_xticks(
+        ticks=dataset.index,
+        labels=dataset.index,
+        ha='center'
+    )
+
+    plt.tight_layout()
+    plt.savefig('files/input/plots/news.png')
+
+    return print('gráfico creado con éxito')
+
+
+if __name__ == "__main__":
+    pregunta_01()
